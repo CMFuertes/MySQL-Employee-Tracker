adf1d23de08cb9e4ce06ae94173a3257b825349b
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-08-22 08:16:43 -0400

    create table for role

--- schema.sql
+++ schema.sql
@@ -2,3 +2,18 @@ DROP DATABASE IF EXISTS etracker_db;
 CREATE DATABASE etracker_db;
 
 USE etracker_db;
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  departmentName VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL(10,2) NOT NULL, 
+    departmentName INT NOT NULL, 
+    PRIMARY KEY (id)
+);
+
